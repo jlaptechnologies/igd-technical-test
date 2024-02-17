1b50076f4f0a6a43a021dc2bbc32e773a69eb149
Justin Patchett <justin.patchett@jlaptech.co.uk>
2024-02-17 13:51:52 +0000

    Delete this file

--- src/resources/views/partials/member/memberlink.blade.php
+++
@@ -1,1 +0,0 @@
-<a href="{{ route('member.profile', ['id' => $member->id]) }}" title="{{ $member->firstName }} {{ $member->lastName }} Profile Page">{{ $member->firstName }} {{ $member->lastName }}</a>
